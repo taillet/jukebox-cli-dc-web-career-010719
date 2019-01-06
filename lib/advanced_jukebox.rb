6474387dbce6932cd56ba5407a104b66c43e4390
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 00:59:22 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,16 +2,6 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
- my_songs = {
- "Go Go GO" => '/home/taillet/jukebox-cli/audio/Emerald-Park/01.mp3',
- "LiberTeens" => '/home/taillet/jukebox-cli/audio/Emerald-Park/02.mp3',
- "Hamburg" =>  '/home/taillet/jukebox-cli/audio/Emerald-Park/03.mp3',
- "Guiding Light" => '/home/taillet/jukebox-cli/audio/Emerald-Park/04.mp3',
- "Wolf" => '/home/taillet/jukebox-cli/audio/Emerald-Park/05.mp3',
- "Blue" => '/home/taillet/jukebox-cli/audio/Emerald-Park/06.mp3',
- "Graduation Failed" => '/home/taillet/jukebox-cli/audio/Emerald-Park/07.mp3'
- }
-
 def help
   #this method should be the same as in jukebox.rb
   puts "I accept the following commands:
@@ -40,13 +30,45 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
+    choice = gets.chomp
+    e = my_songs.size
+    if my_songs.include?(choice) == false
+      puts "Invalid input, please try again"
+      choice = gets.chomp
+    end
+    if my_songs.include?(choice) == true
+      filename = my_songs[choice]
+      puts "#{filename}"
+    #  system "open #{filename}"
+    end
 
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  help
+  puts "Please enter a command:"
+  input = gets.chomp
+  while input != "exit"
+      if input == "help"
+        help
+      elsif input == "list"
+        list(my_songs)
+      elsif input == "play"
+        puts "Please enter a song name:"
+        play(my_songs)
+      elsif input != "help" || input != "list" || input != "play"
+        puts "Please enter a command:"
+      end
+    puts "Please enter a command:"
+    input = gets.chomp
+  end
+  if input == "exit"
+    exit_jukebox
+  end
 end
